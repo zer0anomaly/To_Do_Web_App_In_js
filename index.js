c50be7d27cbb30c37c041e0f347d0a137d5e7dee
zer0anomaly <zeroanomale@gmail.com>
2025-06-01 15:28:46 +0500

    new and updated one

--- index.js
+++ index.js
@@ -1,22 +1,75 @@
 let list = [];
+let completedlist = [];
 
 let newtask = document.getElementById('new-task')
 let addtask = document.getElementById('add-task')
 let tasklist= document.getElementById('task-list')
+let numberoftasks = document.getElementById('task-count')
+let completedbutton = document.getElementById('completedbutton')
+let activebutton = document.getElementById('activebutton')
+let completedtaskcount = document.getElementById('completedtask-count')
+let clearcompletedbutton = document.getElementById('clear-completed')
 
 addtask.addEventListener('click', () => {
 	let task = newtask.value.trim();
-
-	if(tasl){
+	if (task){
 		list.unshift(task)
 		newtask.value = ''
 		functionone()
-	}
+		functiontwo()
+		functionthree()
+	}	
 })
 
 function functionone(){
 	tasklist.innerHTML = '';
-	let li = document.createElement('li');
-	li.textContent = list;
-	tasklist.appendChild(li);
+	for (let i = 0; i < list.length; i++){
+		let li = document.createElement('li');
+		let cpltbutton = document.createElement('button');
+		cpltbutton.textContent = "Completed"
+		li.textContent = list[i];
+		li.appendChild(cpltbutton) 
+		tasklist.appendChild(li);
+		cpltbutton.addEventListener('click', () => {
+			let element = list.splice(i, 1)[0];
+			completedlist.unshift(element)
+			newtask.value = ''
+			functiontwo()
+			functionone()
+			functionthree()
+		})
+	}
 }
+function functiontwo () {
+	numberoftasks.textContent = list.length + " Tasks Left"
+}
+function functionthree (){
+	completedtaskcount.textContent = completedlist.length + " Tasks Completed"
+}
+finishedbutton.addEventListener('click', () => {
+	tasklist.innerHTML = '';
+	for (i = 0; i < completedlist.length; i++){
+		let li = document.createElement('li');
+		li.textContent = completedlist[i]
+		tasklist.appendChild(li)
+		functionthree()
+	}
+})
+
+activebutton.addEventListener('click', () => {
+	tasklist.innerHTML = '';
+	for (let i = 0; i < list.length; i++){
+		let li = document.createElement('li');
+		let cpltbutton = document.createElement('button');
+		cpltbutton.textContent = "Completed"
+		li.textContent = list[i];
+		li.appendChild(cpltbutton) 
+		tasklist.appendChild(li);
+		functionone()
+	}
+})
+clearcompletedbutton.addEventListener('click', () => {
+	completedlist.length = 0;
+	functionthree()
+	tasklist.innerHTML = '';
+})
